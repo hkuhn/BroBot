5422203058e158569af4679bbe9d8a78a8a5f400
Hobey Kuhn <hkuhn@umich.edu>
2013-04-02 17:51:15 -0400

    ready for thresholding debuggin

--- Away/Vision/RingDetection/RingDetectionDetector.java
+++ Away/Vision/RingDetection/RingDetectionDetector.java
@@ -7,6 +7,7 @@ public class RingDetectionDetector {
 
 
     // args
+    private BufferedImage im;
 	private int width;
 	private int height;
 	private int[][] binarizedImageArray;	// C-ORDERED MATRIX (ROW MAJOR)
@@ -24,7 +25,24 @@ public class RingDetectionDetector {
 
 	// PRIVATE METHODS
 	private void binarizeImage() {
-		// scan image, test threshold		
+		// scan image, test threshold
+        int[] imageArray = ((DataBufferInt) im.getRaster().getDataBuffer()).getData();
+        
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                
+                int rgb = imageArray[x + y*width];
+                int r = (rgb >> 16) & 0xff;
+                int g = (rgb >> 8) & 0xff;
+                int b = (rgb) & 0xff;
+                int avg = (r + g + b) / 3;
+                if (avg >= t) {
+                    binarizedImageArray[y][x] = 1;
+                    //im.setRGB(x,y, 0xffffffff);   //fill white
+                }
+                
+            }
+        }
 
 	}
     
@@ -32,6 +50,7 @@ public class RingDetectionDetector {
     // RUN DETECTION METHOD
     public void runDetection(BufferedImage im, int thresh) {
         // retrieve image properties & initialize
+        this.im = im;
 		this.width = im.getWidth();
 		this.height = im.getHeight();
 		this.binarizedImageArray = new int[height][width];
@@ -39,6 +58,11 @@ public class RingDetectionDetector {
 		// run thresholding, signal matches on binarized image
 		binarizeImage();
         
+    }
+    
+    // ACCESS METHODS
+    public BufferedImage getBinarizedImage() {
+        final WriteableRaster wr = new WriteableRaster();
     }
     
 
