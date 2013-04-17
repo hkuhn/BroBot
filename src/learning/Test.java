79e07d4d4c58dd3bc5924805f1ac16e3ef2869f4
Katherine Liu <KatherineLiu@67-194-204-119.wireless.umnet.umich.edu>
2013-04-17 00:56:50 -0400

    test file with test data

--- src/learning/Test.java
+++ src/learning/Test.java
@@ -1,6 +1,7 @@
 package learning;
 
 import april.jmat.Matrix;
+import java.lang.Math;
 
 public class Test {
 
@@ -8,7 +9,15 @@ public class Test {
         double [][] xData = {{1, 4}, {3, 7}, {10, -19}};
         double [][] yData = {{9}, {17}, {-28}};
 
-        LinearRegression r = new LinearRegression(new Matrix(xData), new Matrix(yData));
+	double[][] angles = { {0, 0, 0},
+							{ 0, 0, Math.PI/8},
+							{ 0, 0, Math.PI/4},
+							{ Math.PI/8, 0, 0},
+							{ -Math.PI/4, 0, 0}};
+
+	double[][] distances = { {79}, {60}, {33}, {38}, {67.5} };
+
+        LinearRegression r = new LinearRegression(new Matrix(angles), new Matrix(distances));
         LinearEquation eq = r.getResultantLinearEquation();
 
         for ( int i = 0; i < xData.length; i++ ) {
